4f1d9d0b06c50fceb93386d24b46fcbadd24052f
Ted Ralphs <ted@lehigh.edu>
2011-08-06 16:12:15 +0000

    Updates to build files and changes to automatically generated headers

--- CoinMP/src/CoinMPConfig.h
+++ CoinMP/src/CoinMPConfig.h
@@ -1,5 +1,10 @@
 /*
- * Include file for the configuration of Cbc.
+ * Include file for the configuration of CoinMP.
+ *
+ * On systems where the code is configured with the configure script
+ * (i.e., compilation is always done with HAVE_CONFIG_H defined), this
+ * header file includes the automatically generated header file, and
+ * undefines macros that might configure with other Config.h files.
  *
  * On systems where the code is configured with the configure script
  * (i.e., compilation is always done with HAVE_CONFIG_H defined), this
@@ -10,88 +15,30 @@
  * Developer Studio), a header files is included to define those
  * macros that depend on the operating system and the compiler.  The
  * macros that define the configuration of the particular user setting
- * (e.g., presence of other COIN packages or third party code) are set
- * here.  The project maintainer needs to remember to update this file
- * and choose reasonable defines.  A user can modify the default
- * setting by editing this file here.
+ * (e.g., presence of other COIN-OR packages or third party code) are set
+ * by the files config_*default.h. The project maintainer needs to remember
+ * to update these file and choose reasonable defines.
+ * A user can modify the default setting by editing the config_*default.h files.
  *
  */
 
 #ifndef __COINMPCONFIG_H__
 
 #ifdef HAVE_CONFIG_H
+#ifdef COINMP_BUILD
+#include "config.h"
+#else
 #include "config_coinmp.h"
-
-/* undefine macros that could conflict with those in other config.h
-   files */
-#undef PACKAGE
-#undef PACKAGE_BUGREPORT
-#undef PACKAGE_NAME
-#undef PACKAGE_STRING
-#undef PACKAGE_TARNAME
-#undef PACKAGE_VERSION
-#undef VERSION
+#endif
 
 #else /* HAVE_CONFIG_H */
 
-/* include the COIN-wide system specific configure header */
-#include "configall_system.h"
-
-/***************************************************************************/
-/*             HERE DEFINE THE CONFIGURATION SPECIFIC MACROS               */
-/***************************************************************************/
-
-/* Define to the debug sanity check level (0 is no test) */
-#define COIN_COINMP_CHECKLEVEL 0
-
-/* Define to 1 if the FlopCpp package is used */
-#define COIN_HAS_COINMP 1
-
-/* Define to 1 if the Cbc package is used */
-#define COIN_HAS_CBC 1
-
-/* Define to 1 if the Cgl package is used */
-#define COIN_HAS_CGL 1
-
-/* Define to 1 if the Clp package is used */
-#define COIN_HAS_CLP 1
-
-/* Define to 1 if the CoinUtils package is used */
-#define COIN_HAS_COINUTILS 1
-
-/* Define to 1 if the Osi package is used */
-#define COIN_HAS_OSI 1
-
-/* Define to 1 if the Vol package is used */
-/* #define COIN_HAS_VOL 1 */
-
-/* Define to 1 if the Cplex package is used */
-/* #undef COIN_HAS_CPX */
-
-/* Define to 1 if the Dylp package is used */
-/* #undef COIN_HAS_DYLP */
-
-/* Define to 1 if the FortMP package is used */
-/* #undef COIN_HAS_FMP */
-
-/* Define to 1 if the Glpk package is used */
-/* #undef COIN_HAS_GLPK */
-
-/* Define to 1 if the Mosek package is used */
-/* #undef COIN_HAS_MSK */
-
-/* Define to 1 if the Osl package is used */
-/* #undef COIN_HAS_OSL */
-
-/* Define to 1 if the Soplex package is used */
-/* #undef COIN_HAS_SPX */
-
-/* Define to 1 if the Sym package is used */
-/* #undef COIN_HAS_SYM */
-
-/* Define to 1 if the Xpress package is used */
-/* #undef COIN_HAS_XPR */
+#ifdef CBC_BUILD
+#include "config_default.h"
+#else
+#include "config_coinmp_default.h"
+#endif
 
 #endif /* HAVE_CONFIG_H */
 
-#endif /*__HAVE_COINUTILS_CONFIG_H__*/
+#endif /*__COINMPCONFIG_H__*/
