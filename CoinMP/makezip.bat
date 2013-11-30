4a3f4077ee21496c04c4f99f6c16529b8287d0b9
Bjarni Kristjansson <bjarni@maximalsoftware.com>
2013-11-30 21:51:17 +0000

    Changed location of the example files in CoinMP.zip

--- CoinMP/makezip.bat
+++ CoinMP/makezip.bat
@@ -4,6 +4,8 @@ cd CoinMP_zip
 
 mkdir x64
 mkdir src
+mkdir examples
+cd examples
 mkdir vb6
 mkdir csharp
 cd csharp
@@ -11,49 +13,48 @@ mkdir Properties
 mkdir bin
 cd bin
 mkdir Release
-cd ..\..
+cd ..\..\..
 
 copy ..\MSVisualStudio\v9\release\coinmp.dll
 copy ..\MSVisualStudio\v9\release\coinmp.exp
 copy ..\MSVisualStudio\v9\release\coinmp.lib
 copy ..\MSVisualStudio\v9\release\unittest.exe
 
-copy ..\MSVisualStudio\v9\x64\release\coinmp.dll x64
-copy ..\MSVisualStudio\v9\x64\release\coinmp.exp x64
-copy ..\MSVisualStudio\v9\x64\release\coinmp.lib x64
-copy ..\MSVisualStudio\v9\x64\release\unittest.exe x64
+copy ..\src\CoinMP.h
 
 copy ..\examples\vb6\CoinMPTestVB6.exe
 
 copy ..\examples\csharp\bin\Release\CoinMPTestCS9.exe
 
-copy ..\test\unittest.cpp
-
-copy ..\examples\example.c
-
-copy ..\src\CoinMP.h
+copy ..\MSVisualStudio\v9\x64\release\coinmp.dll   x64
+copy ..\MSVisualStudio\v9\x64\release\coinmp.exp   x64
+copy ..\MSVisualStudio\v9\x64\release\coinmp.lib   x64
+copy ..\MSVisualStudio\v9\x64\release\unittest.exe x64
 
 copy ..\src\*.cpp      src
 copy ..\src\*.c        src
 copy ..\src\*.h        src
 copy ..\src\coinmp.def src
 
-copy ..\examples\vb6\*.bas              vb6
-copy ..\examples\vb6\CoinMPTestVB6.vbp  vb6
-copy ..\examples\vb6\CoinMPTestVB6.exe  vb6
-copy ..\examples\vb6\FrmCoinMP.frm      vb6
+copy ..\test\unittest.cpp   examples
+copy ..\examples\example.c  examples
+
+copy ..\examples\vb6\*.bas              examples\vb6
+copy ..\examples\vb6\CoinMPTestVB6.vbp  examples\vb6
+copy ..\examples\vb6\CoinMPTestVB6.exe  examples\vb6
+copy ..\examples\vb6\FrmCoinMP.frm      examples\vb6
 
 
-copy ..\examples\csharp\*.cs         csharp
-copy ..\examples\csharp\*.csproj     csharp
-copy ..\examples\csharp\*.sln        csharp
-copy ..\examples\csharp\Form1.resx   csharp
+copy ..\examples\csharp\*.cs         examples\csharp
+copy ..\examples\csharp\*.csproj     examples\csharp
+copy ..\examples\csharp\*.sln        examples\csharp
+copy ..\examples\csharp\Form1.resx   examples\csharp
 
-copy ..\examples\csharp\Properties\*.cs                csharp\Properties
-copy ..\examples\csharp\Properties\Resources.resx      csharp\Properties
-copy ..\examples\csharp\Properties\Settings.settings   csharp\Properties
+copy ..\examples\csharp\Properties\*.cs                examples\csharp\Properties
+copy ..\examples\csharp\Properties\Resources.resx      examples\csharp\Properties
+copy ..\examples\csharp\Properties\Settings.settings   examples\csharp\Properties
 
-copy ..\examples\csharp\bin\Release\CoinMPTestCS9.exe  csharp\bin\Release
+copy ..\examples\csharp\bin\Release\CoinMPTestCS9.exe  examples\csharp\bin\Release
 
 cd ..
 
