5dd010b1cc8e290aac543692dbc14e1522ce0602
Ted Ralphs <ted@lehigh.edu>
2015-06-02 16:28:43 +0000

    restoring CoinMP/stable/1.8

--- CoinMP/src/config_coinmp_default.h
+++ CoinMP/src/config_coinmp_default.h
@@ -5,7 +5,7 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define COINMP_VERSION "1.8.2"
+#define COINMP_VERSION "1.8"
 
 /* Major Version number of project */
 #define COINMP_VERSION_MAJOR 1
@@ -14,4 +14,4 @@
 #define COINMP_VERSION_MINOR 8
 
 /* Release Version number of project */
-#define COINMP_VERSION_RELEASE 2
+#define COINMP_VERSION_RELEASE 9999
